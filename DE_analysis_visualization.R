8da1123a1e4cb602e02e9b2d2aaf0fe00e966b62
Yan Li <yli22@bsd.uchicago.edu>
2017-02-24 13:39:01 -0600

    update ol results output

--- DE_analysis_visualization.R
+++ DE_analysis_visualization.R
@@ -8,6 +8,7 @@ resDir <- as.character(options[2])
 print("----")
 workDir <- resDir
 setwd(workDir)
+print(sprintf("Working directory is at %s, the same as results saving directory", resDir))
 
 group1 <- as.character(options[3])
 group2 <- as.character(options[4])
@@ -23,14 +24,14 @@ fc.val <- as.numeric(options[8])
 # resDir <- "/Volumes/yli/DE_tools/DEG_analysis_res/"
 # workDir <- resDir
 # setwd(workDir)
-# group1 <- "Control"
-# group2 <- "Control_Hypothroy"
+# group1 <- "DBDB_Control"
+# group2 <- "DBDB"
 # 
 # samp.cutoff <- 2
 # cpm.val <- 3
 # 
 # padj.val <- 0.05
-# fc.val <- 1.5
+# fc.val <- 1
 ####
 library(ggplot2)
 print("-----")
@@ -51,8 +52,13 @@ res.deseq2.deg <- get.deg(deseq2.res.fname, fc = fc.val, pval = padj.val, method
 res.voom.deg <- get.deg(limmavoom.res.fname, fc = fc.val, pval = padj.val, method = 'voom')
 print(sprintf("No. of DEGs from edgeR = %s, DESeq2 = %s, Limma-voom = %s", 
               length(rownames(res.edgeR.deg)), length(rownames(res.deseq2.deg)), length(rownames(res.voom.deg))))
+print(sprintf("Volcano plots are saved at %s", plotDir))
+###Venn part
 library(gplots)
-plot.name <- paste(plotDir, "/venn.pdf", sep = "")
+olDir <- paste(as.character(workDir), "/ol-res", sep = "")
+if (!dir.exists(olDir)) dir.create(olDir)
+print(sprintf("overlap results are saved at %s", olDir))
+plot.name <- paste(olDir, "/venn.pdf", sep = "")
 ol <- venn(list('edgeR' = rownames(res.edgeR.deg),
                 'DESeq2' = rownames(res.deseq2.deg),
                 'Limma-voom' = rownames(res.voom.deg)), show.plot = F)
@@ -60,4 +66,43 @@ pdf(plot.name, height = 5, width = 5)
 par(mar = c(1, 0, 0, 0))
 plot(ol)
 dev.off()
+
+##Write ol results
+ol1 <- cbind(res.edgeR.deg[match(attr(ol, "intersections")$`edgeR:DESeq2:Limma-voom`, rownames(res.edgeR.deg)),],
+             res.voom.deg[match(attr(ol, "intersections")$`edgeR:DESeq2:Limma-voom`, rownames(res.voom.deg)),],
+             res.deseq2.deg[match(attr(ol, "intersections")$`edgeR:DESeq2:Limma-voom`, rownames(res.deseq2.deg)),])
+ol2 <- cbind(res.edgeR.deg[match(attr(ol, "intersections")$`edgeR:Limma-voom`, rownames(res.edgeR.deg)),],
+             res.voom.deg[match(attr(ol, "intersections")$`edgeR:Limma-voom`, rownames(res.voom.deg)),])
+ol3 <- cbind(res.edgeR.deg[match(attr(ol, "intersections")$`edgeR:DESeq2`, rownames(res.edgeR.deg)),],
+             res.deseq2.deg[match(attr(ol, "intersections")$`edgeR:DESeq2`, rownames(res.deseq2.deg)),])
+ol4 <- cbind(res.voom.deg[match(attr(ol, "intersections")$`DESeq2:Limma-voom`, rownames(res.voom.deg)),],
+             res.deseq2.deg[match(attr(ol, "intersections")$`DESeq2:Limma-voom`, rownames(res.deseq2.deg)),])
+ol5 <- res.deseq2.deg[match(attr(ol, "intersections")$`DESeq2`, rownames(res.deseq2.deg)),]
+ol6 <- res.edgeR.deg[match(attr(ol, "intersections")$`edgeR`, rownames(res.edgeR.deg)),]
+ol7 <- res.voom.deg[match(attr(ol, "intersections")$`Limma-voom`, rownames(res.voom.deg)),]
+
+colnames(ol1) <- c(paste("edgeR", colnames(res.edgeR.deg), sep = "_"), paste("voom", colnames(res.voom.deg), sep = "_"), paste("DESeq2", colnames(res.deseq2.deg), sep = "_") )
+colnames(ol2) <- c(paste("edgeR", colnames(res.edgeR.deg), sep = "_"), paste("voom", colnames(res.voom.deg), sep = "_"))
+colnames(ol3) <- c(paste("edgeR", colnames(res.edgeR.deg), sep = "_"), paste("DESeq2", colnames(res.deseq2.deg), sep = "_"))
+colnames(ol4) <- c(paste("voom", colnames(res.voom.deg), sep = "_"), paste("DESeq2", colnames(res.deseq2.deg), sep = "_"))
+colnames(ol5) <- colnames(res.deseq2.deg)
+colnames(ol6) <- colnames(res.edgeR.deg)
+colnames(ol7) <- colnames(res.voom.deg)
+
+ol1.save <- ol1[order(ol1$`edgeR_PValue`), c(1,4,5,7,10,11,15,18,19)]
+ol2.save <- ol2[order(ol2$`edgeR_PValue`), c(1,4,5,7,10,11)]
+ol3.save <- ol3[order(ol3$`edgeR_PValue`), c(1,4,5,8,11,12)]
+ol4.save <- ol4[order(ol4$`voom_P.Value`), c(1,4,5,9,12,13)]
+ol5.save <- ol5[order(ol5$`pvalue`), c(2,5,6)]
+ol6.save <- ol6[order(ol6$`PValue`), c(1,4,5)]
+ol7.save <- ol7[order(ol7$`P.Value`), c(1,4,5)]
+
+write.table(ol1.save, file = paste(olDir, "all3_overlap.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+write.table(ol2.save, file = paste(olDir, "edger_voom_overlap_only.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+write.table(ol3.save, file = paste(olDir, "edger_deseq2_overlap_only.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+write.table(ol4.save, file = paste(olDir, "voom_deseq2_overlap_only.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+write.table(ol5.save, file = paste(olDir, "deseq2_only.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+write.table(ol6.save, file = paste(olDir, "edgeR_only.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+write.table(ol7.save, file = paste(olDir, "voom_only.txt", sep = "/"), sep ="\t", quote = F, row.names = T, col.names = NA)
+
 print("-----")
